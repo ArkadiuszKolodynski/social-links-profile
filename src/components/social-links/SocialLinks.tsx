cc8cdb41d871b174b71f43944a85acb246c58e80
ArkadiuszKolodynski <ArkadiuszKolodynski@users.noreply.github.com>
2024-06-26 13:15:02 +0200

    feat: add social links container component

---
+++ src/components/social-links/SocialLinks.tsx
@@ -0,0 +1,32 @@
+import avatar from "../../assets/images/avatar-jessica.jpeg";
+import "./SocialLinks.scss";
+
+function SocialLinks() {
+  const socialLinks = [
+    "GitHub",
+    "Frontend Mentor",
+    "LinkedIn",
+    "Twitter",
+    "Instagram",
+  ];
+
+  return (
+    <div id="social-links">
+      <img src={avatar} alt="avatar" width={88} />
+      <div>
+        <h1>Jessica Randall</h1>
+        <h2>London, United Kingdom</h2>
+      </div>
+      <q>Front-end developer and avid reader.</q>
+      <ul id="social-links-list">
+        {socialLinks.map((link) => (
+          <li key={link}>
+            <a href="#">{link}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SocialLinks;
